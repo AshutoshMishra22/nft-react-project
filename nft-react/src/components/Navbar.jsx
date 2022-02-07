f32f5aa3b9d615eadbdda7ea5522520d89ebdca4
ashutosh001 <63530972+AshutoshMishra22@users.noreply.github.com>
2022-02-08 00:08:09 +0530

    Login Page added

--- nft-react/src/components/Navbar.jsx
+++ nft-react/src/components/Navbar.jsx
@@ -2,19 +2,48 @@ import React, { useState } from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 
-import logo from "../images/logo.png";
+import logo from "../icons/logo.png";
+
+const Input = ({
+  placeholder,
+  type,
+  name,
+  value,
+  classProps,
+  handleChange,
+}) => (
+  <input
+    placeholder={placeholder}
+    type={type}
+    name={name}
+    value={value}
+    onChange={(e) => handleChange(e, name)}
+    step='0.0001'
+    className={`my-2 w-full rounded-sm p-2 outline-none bg-transparent border-[#3d4f7c] text-white text-sm${
+      type === "number" ? "appearance-none" : ""
+    } ${classProps}`}
+  />
+);
 
 const NavbarItem = ({ title, classProps }) => {
   return <li className={`mx-4 cursor-pointer ${classProps}`}>{title}</li>;
 };
 const Navbar = () => {
-  const [state, setState] = useState({ toggleMenu: false });
-  const { toggleMenu } = state;
+  const [state, setState] = useState({
+    toggleMenu: false,
+    toggleLogin: false,
+    loginTry: false,
+  });
+  const { toggleMenu, toggleLogin, loginTry } = state;
 
   function updateState(stateKey, stateValue) {
     setState((prevState) => {
       return { ...prevState, [stateKey]: stateValue };
     });
+  }
+
+  function handleChange(e, name) {
+    updateState(name, e.target.value);
   }
 
   return (
@@ -26,7 +55,10 @@ const Navbar = () => {
         {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
           <NavbarItem key={item + index} title={item} />
         ))}
-        <li className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]'>
+        <li
+          className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]'
+          onClick={() => updateState("toggleLogin", true)}
+        >
           Login
         </li>
       </ul>
@@ -45,11 +77,10 @@ const Navbar = () => {
           />
         )}
         {toggleMenu && (
-          <ul className='z-10 fixed -right-2 top-0 right-2 p-3  w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in'>
+          <ul className='z-10 fixed right-2 top-0 right-2 p-3  w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in'>
             <li className='text-xl w-full my-2'>
               <AiOutlineClose
                 onClick={() => updateState("toggleMenu", false)}
-                classProps
               />
             </li>
             {["Market", "Exchange", "Tutorials", "Wallets"].map(
@@ -61,9 +92,52 @@ const Navbar = () => {
                 />
               )
             )}
+            <li
+              className='my-2 text-lg mx-4 cursor-pointer'
+              onClick={() => {
+                updateState("toggleLogin", true);
+                updateState("toggleMenu", false);
+              }}
+            >
+              Login
+            </li>
           </ul>
         )}
       </div>
+      {toggleLogin && (
+        <div className='fixed blue-glassmorphism z-20 w-[100%] h-[100%] bottom-0'>
+          <div className='text-xl w-full m-6 text-white cursor-pointer'>
+            <AiOutlineClose onClick={() => updateState("toggleLogin", false)} />
+          </div>
+          <div className='text-white animate-scale-up-center grid place-items-center relative h-[80%] w-[100%]'>
+            <div className='p-5 flex flex-col justify-evenly items-center gradient-bg-login rounded-lg'>
+              <div className='w-full mt-5 mx-8px'>
+                <Input
+                  placeholder='Email'
+                  name='email'
+                  type='text'
+                  handleChange={handleChange}
+                />
+                <Input
+                  placeholder='Security key'
+                  name='key'
+                  type='password'
+                  handleChange={handleChange}
+                />
+              </div>
+              <button
+                type='button'
+                onClick={() => updateState("loginTry", true)}
+                className={`text-white w-full my-8 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer ${
+                  loginTry ? "animate-bg-pan-right" : ""
+                }`}
+              >
+                Login
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
