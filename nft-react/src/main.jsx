bc8adbe0e4ad55a423e201572d724b4cc49c8fcc
ashutosh001 <63530972+AshutoshMishra22@users.noreply.github.com>
2022-02-15 20:32:13 +0530

    configured to main

--- nft-react/src/main.jsx
+++ nft-react/src/main.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
 import { TransactionProvider } from "./context/TransactionContext";
+import store from "./redux/store";
 
 ReactDOM.render(
-  <TransactionProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </TransactionProvider>,
+  <Provider store={store}>
+    <TransactionProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </TransactionProvider>
+  </Provider>,
   document.getElementById("root")
 );
